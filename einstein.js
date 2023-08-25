42d0ac085cf9ad22a70a2119b356a969d33bae2a
Dima163163 <khlopovdima@gmail.com>
2023-08-25 13:38:50 +0400

    fix

--- einstein.js
+++ einstein.js
@@ -5,4 +5,4 @@ const speedLight = 3e8;
 
 const energyOfMyBody = myWight * Math.pow(speedLight, 2);
 
-console.log('Энергия тела: ' + energyOfMyBody + ' Джоулей')
+console.log('Энергия тела: ' + energyOfMyBody + ' Джоулей');
