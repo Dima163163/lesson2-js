9afc5c9eed065fd1d70876c43d010457e475f3cd
Dima163163 <khlopovdima@gmail.com>
2023-08-25 13:30:41 +0400

    lesson-2

---
+++ einstein.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const myWight = 74;
+const speedLight = 3e8;
+
+const energyOfMyBody = myWight * Math.pow(speedLight, 2);
+
+console.log('Энергия тела: ' + energyOfMyBody + ' Джоулей')
---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const nameProduct = prompt('Введите наименование товара:', '');
+const amountProduct = +prompt('Введите количество товара:', '');
+const categoryProduct = prompt('Введите категорию товара:', '');
+const priceProduct = +prompt('Введите цену товара товара:', '');
+
+console.log('Тип даных amountProduct:', typeof(amountProduct));
+console.log('Тип даных priceProduct:', typeof(priceProduct));
+
+console.log('Наименование товара: ' + nameProduct);
+console.log('Общая сумма товара: ' + (amountProduct * priceProduct));
+console.log(`На складе ${amountProduct} еденицы товара "${nameProduct}" на сумму ${priceProduct * amountProduct} рублей.`);
